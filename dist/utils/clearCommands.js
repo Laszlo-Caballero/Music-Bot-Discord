c6bab923b7aba824f65623f830f8c7d900c19b51
laszlocaballero <laszlocaballero@gmail.com>
2024-10-27 21:22:52 -0500

    remove dist

--- dist/utils/clearCommands.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clearCommands = clearCommands;
-async function clearCommands(client) {
-    await client.clearApplicationCommands();
-    console.log("se elimino los comandos");
-}
